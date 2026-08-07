b072b26b2a62202be3c639b9934c0b31fc951502
Zafra Ziaudeen <zafraziaudeen@gmail.com>
2026-08-07 21:10:00 +0530

    feat: add environment configuration, error handling, and logging

---
+++ src/middleware/validate.ts
@@ -0,0 +1,11 @@
+import type { RequestHandler } from 'express';
+import type { z } from 'zod';
+//this function returns a middleware function that validates the request body against the provided Zod schema.
+//  If the validation passes, it assigns the parsed body to req.body and calls next() to proceed to the next middleware or route handler. 
+// If the validation fails, it will throw an error, which can be handled by an error-handling middleware.
+export function validateBody<S extends z.ZodType>(schema: S): RequestHandler {
+    return (req, res, next) => {
+        req.body = schema.parse(req.body);
+        next();
+    }
+}
